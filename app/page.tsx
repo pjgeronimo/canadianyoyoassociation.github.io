70179a46da388d7378e92cecf89f536631e743c3
Paolo Geronimo <paolojose.geronimo@ucalgary.ca>
2025-12-17 18:20:29 -0700

    some homepage styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,62 @@
 "use client";
 import Link from "next/link";
-import Image from "next/image";
+import Image from "react-bootstrap/Image";
 import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Home() {
   return (
     <>
-      <Image
-        src="/assets/logos/2026natslogo.png"
-        width={800}
-        height={800}
-        alt="2026 Canadian Nationals Logo"
-      />
-      <h1>Canadian Nationals 2025</h1>
-      <p>
-        Welcome to the official website of the Canadian National Return Top
-        Championship 2026! Some of the finest return top players from all over
-        Canada will be competing for this year's nationals titles in Calgary, AB
-        on Sunday, May 17th.
-      </p>
-      <p>
-        As a member organization of the{" "}
-        <Link href="https://iyyf.org/">International Yo-yo Federation</Link>,
-        the winners of Canadian Nationals will be seeded to the World Yo-yo
-        Contest 2026.
-      </p>
-      <p>
-        This event is family-friendly and open to the public for free.
-        Contestants must register <Link href="/register">here</Link>.
-      </p>
-      <p>
-        To keep up to date or get in touch with us, use the hashtag
-        #CanadaNats2026 and follow us on social media
-      </p>
-      <Button href="https://linktr.ee/canadianyoyoassociation">
-        @canadianyoyoassociation
-      </Button>
+      <Container className="text-center">
+        <Row>
+          <Col>
+            <Image
+              src="/assets/logos/2026natslogo.png"
+              alt="2026 Canadian Nationals Logo"
+              width="40%"
+            />
+          </Col>
+        </Row>
+        <Row>
+          <h1>May 17th, 2026</h1>
+          <h1>Calgary, AB</h1>
+          <p>
+            Welcome to the official website of the Canadian National Return Top
+            Championship 2026! Some of the finest return top players from all
+            over Canada will be competing for this year's nationals titles in
+            Calgary, AB on Sunday, May 17th.
+          </p>
+          <p>
+            As a member organization of the{" "}
+            <Link href="https://iyyf.org/" className="link">
+              International Yo-yo Federation
+            </Link>
+            , the winners of Canadian Nationals will be seeded to the World
+            Yo-yo Contest 2026.
+          </p>
+          <p>
+            This event is family-friendly and open to the public for free.
+            Contestants must register{" "}
+            <Link href="/register" className="link">
+              here
+            </Link>
+            .
+          </p>
+          <p>
+            To keep up to date or get in touch with us, use the hashtag
+            #CanadaNats2026 and follow us on social media
+          </p>
+        </Row>
+
+        <Button
+          href="https://linktr.ee/canadianyoyoassociation"
+          className="socials-btn"
+        >
+          @canadianyoyoassociation
+        </Button>
+      </Container>
     </>
   );
 }
