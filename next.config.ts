0716712a756e2e3a857ed292db8a70736288f5a2
Evan Cui <evan@good-meowning.com>
2025-12-17 21:16:46 -0500

    fix archive endpoints with redirects

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,32 @@ const nextConfig: NextConfig = {
   /* config options here */
   //output: "export",
   trailingSlash: true,
+
+  // fix archived endpoints
+  async redirects() {
+    return [
+      {
+        source: "/2023",
+        destination: "/2023/index.html",
+        permanent: true
+      },
+      {
+        source: "/crto2024",
+        destination: "/crto2024/index.html",
+        permanent: true
+      },
+      {
+        source: "/2024",
+        destination: "/2024/index.html",
+        permanent: true
+      },
+      {
+        source: "/2025",
+        destination: "/2025/index.html",
+        permanent: true
+      }
+    ];
+  }
 };
 
 export default nextConfig;
