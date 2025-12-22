a5650d70bdcd9c5eb826800544b727b700472ad3
Paolo Geronimo <paolojose.geronimo@ucalgary.ca>
2025-12-22 13:52:51 -0700

    intl and junior div content

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -14,6 +14,7 @@ const Rules = () => {
           Open divisions. Please see the <Link href="/faq">FAQ</Link> to check
           your eligibility to compete in a Canadian Championship Division.
         </p>
+        <p>Winners will be seeded to the 2026 World Yo-yo contest.</p>
         <p>
           Each contestant will perform a freestyle routine choreographed to
           music up to the for the following lengths in each division/round:
@@ -72,13 +73,95 @@ const Rules = () => {
             inappropriate music is subject to disqualification.
           </p>
           <p>
-            Please see the{" "}
+            Please see{" "}
             <Link href="https://iyyf.org/wyyc2025-rules/2025-freestyle-music/">
               IYYF's Guidelines for Music Choice
             </Link>{" "}
             to determine if your music is appropriate.
           </p>
         </Alert>
+        <h1>International Division Freestyles</h1>
+        <p>
+          Non Canadian citizens or residents are welcome to compete in the
+          International Division. Please see the <Link href="/faq">FAQ</Link> to
+          determine if you are eligible to compete in the International
+          Division.
+        </p>
+        <p>
+          International Division freestyles will be a 3 minute freestyle (no
+          preliminaries) where each contestant must choose ONE of the following
+          styles:
+        </p>
+        <ul>
+          <li>1A: String trick freestyle with one long-spinning yo-yo</li>
+          <li>2A: Looping trick freestyle with two responsive yo-yos</li>
+          <li>3A: String Trick freestyle with two long-spinning yo-yos</li>
+          <li>4A: Freestyle with off string yo-yo(s)</li>
+          <li>5A: Freestyle with counter-weight yo-yo(s)</li>
+        </ul>
+        <p>
+          Contestants will upload their own music and must also follow{" "}
+          <Link href="https://iyyf.org/wyyc2025-rules/2025-freestyle-music/">
+            IYYF's Guidelines for Music Choice
+          </Link>
+          .
+        </p>
+        <p>
+          Winners of the International Division will not be seeded to the 2026
+          World Yo-yo contest.
+        </p>
+        <h1>Junior Division Freestyles</h1>
+        <p>
+          The Junior Division is for young players of any nationality who want
+          to experience the fun of performing on stage in a more relaxed format.
+        </p>
+        <p>
+          In order to be eligible to compete in this division, the contestant
+          must
+        </p>
+        <ul>
+          <li>Be age 13 or younger as of May 17, 2026</li>
+          <li>
+            Have never advanced to a 1A finals of any National-level competition
+          </li>
+          <li>
+            Not also enter a Canadian Championship or International division
+          </li>
+        </ul>
+        <p>
+          Junior Division freestyles are 1 minute long, and only the 1A style of
+          play is allowed. The scoring criteria is similar to the{" "}
+          <Link href="https://iyyf.org/wyyc2025-rules/">
+            2025 World Yoyo contest
+          </Link>
+          , but includes the following modifications in order to emphasize the
+          performance aspect of the freestyles:
+        </p>
+        <ul>
+          <li>
+            Technical Execution (TE) and Freestyle Evaluation (FE) will each be
+            50% of the final score
+          </li>
+          <li>
+            There will be no negative points in TE, and there will be no major
+            deductions
+          </li>
+          <li>
+            Choreography (MU1) and Body Control (BDY) will be the only FE
+            categories
+          </li>
+        </ul>
+        <p>
+          Contestants will upload their own music and must also follow{" "}
+          <Link href="https://iyyf.org/wyyc2025-rules/2025-freestyle-music/">
+            IYYF's Guidelines for Music Choice
+          </Link>
+          .
+        </p>
+        <p>
+          Winners of the Junior Division will not be seeded to the 2026 World
+          Yo-yo contest.
+        </p>
       </Container>
     </>
   );
