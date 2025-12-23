5fe6b30c5b9b4a0559c34af4db586ee2013a45aa
Evan Cui <evan@good-meowning.com>
2025-12-22 20:29:15 -0500

    add schedule and results

---
+++ app/results/page.tsx
@@ -0,0 +1,26 @@
+import { Container } from "react-bootstrap";
+
+const Results = () => {
+  return (
+    <Container>
+      <h1>Results - 1A division Finals</h1>
+      <p>i win 😎</p>
+      <img src="/2025/result_files/results_1Afinal.png" width="800px" />
+
+      <h1>Results - Open division</h1>
+      <img src="/2025/result_files/results_open.png" width="800px" />
+
+      <h1>Results - Junior division</h1>
+      <img src="/2025/result_files/results_junior.png" width="400px" />
+
+      <h1>Results - International division</h1>
+      <img src="/2025/result_files/results_international.png" width="800px" />
+
+      <h1>Results - 1A division Preliminaries</h1>
+      <p>Top 10 to advance to finals</p>
+      <img src="/2025/result_files/results_1Aprelim.png" width="800px" />
+    </Container>
+  );
+};
+
+export default Results;
--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,7 +1,69 @@
+import Link from "next/link";
 import React from "react";
+import { Container, Table } from "react-bootstrap";
 
 const Schedule = () => {
-  return <div>Schedule</div>;
+  return (
+    <Container>
+      <h1>Schedule</h1>
+
+      <p>
+        Canadian Nationals is a one-day event starting at 10am and ending at
+        5:30pm. The event is open to the public and{" "}
+        <strong>FREE to attend</strong> for spectators, so bring your friends
+        and family!!
+      </p>
+
+      <p>
+        See the <Link href="results">results</Link> page for the order of
+        freestyle performances.
+      </p>
+
+      <Table striped bordered hover>
+        <tbody>
+          <tr>
+            <td>10:00</td>
+            <td>Doors Open</td>
+          </tr>
+          <tr>
+            <td>11:00</td>
+            <td>Championship 1A division - Preliminaries</td>
+          </tr>
+          <tr>
+            <td>12:00</td>
+            <td>Lunch</td>
+          </tr>
+          <tr>
+            <td>13:30</td>
+            <td>Junior division</td>
+          </tr>
+          <tr>
+            <td>14:00</td>
+            <td>Championship Open and International divisions</td>
+          </tr>
+          <tr>
+            <td>15:00</td>
+            <td>Championship 1A division - Finals</td>
+          </tr>
+          <tr>
+            <td>17:00</td>
+            <td>Results and awards</td>
+          </tr>
+        </tbody>
+      </Table>
+
+      <p>
+        To receive the latest updates, make sure to follow CANYA on social media
+      </p>
+      <Link
+        id="socials-button"
+        href="https://linktr.ee/canadianyoyoassociation"
+        target="_blank"
+      >
+        @canadianyoyoassociation
+      </Link>
+    </Container>
+  );
 };
 
 export default Schedule;
