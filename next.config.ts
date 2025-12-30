a6e82df7624f5ac686850dee467c24aae8e5a802
Paolo Geronimo <paolojose.geronimo@ucalgary.ca>
2025-12-29 22:48:29 -0700

    static deployment fix

--- next.config.ts
+++ next.config.ts
@@ -6,32 +6,6 @@ const nextConfig: NextConfig = {
 
   // remove trailing slash for navbar active highlight
   trailingSlash: false,
-
-  // fix archived endpoints
-  async redirects() {
-    return [
-      {
-        source: "/2023",
-        destination: "/2023/index.html",
-        permanent: true
-      },
-      {
-        source: "/crto2024",
-        destination: "/crto2024/index.html",
-        permanent: true
-      },
-      {
-        source: "/2024",
-        destination: "/2024/index.html",
-        permanent: true
-      },
-      {
-        source: "/2025",
-        destination: "/2025/index.html",
-        permanent: true
-      }
-    ];
-  }
 };
 
 export default nextConfig;
